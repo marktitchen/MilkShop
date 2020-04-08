cfb7d4669b49a62b2618aa4906ee91420613cf3f
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2020-04-08 06:18:36 +0100

    Design Complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -314,7 +314,7 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(779, 206);
+			this.button1.Location = new System.Drawing.Point(779, 169);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(110, 31);
 			this.button1.TabIndex = 4;
